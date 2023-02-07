14e178e19d7be56d1f839fdb9c0eb5cd0bcba893
Jethro <jethrotiago@hotmail.com>
2023-02-07 20:54:14 -0300

    Criado códigos para o About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,9 +1,37 @@
 import React from 'react';
 import AboutBackground from '../Assets/about-background.png';
-import AboutBackgroundImage from '..Assets/about-background-image.png'
+import AboutBackgroundImage from '../Assets/about-background-image.png';
+import { BsFillPlayCircleFill } from 'react-icons/bs';
 
 const About = () => {
-  return <div>About</div>
+  return <div className='about-section-container'>
+    <div className='about-background-image-container'>
+        <img src={AboutBackground} alt="Um belo pano quadriculado laranja" />
+    </div>
+    <div className='about-section-image-container'>
+        <img src={AboutBackgroundImage} alt='Imagem de uma pizza saborosa'></img>
+    </div>
+    <div className='about-section-text-container'>
+        <p className='primary-subheading'>Sobre</p>
+        <h1 className='primary-heading'>
+            A vida é melhor com uma pizza!
+        </h1>
+        <p className='primary-text'>
+            A pizza da Express Cajacity pizzaria é considerada como a melhor pizza
+            da região de Cajazeiras e está entre as melhores pizzas da cidade!
+        </p>
+        <p className='primary-text'>
+            Desenvolvida com os melhores ingredientes e com sabores inovadores que 
+            você só vai encontrar aqui com a gente! Peça logo a sua!
+        </p>
+        <div className='about-buttons-container'>
+            <button className='secondary-button'>Conheça os sabores</button>
+            <button className='watch-video-button'>
+                <BsFillPlayCircleFill /> Assista o vídeo!
+            </button>
+        </div>
+    </div>
+  </div>
 };
 
 export default About
