ffedde0db6459bf014d4634f9cb2dde76a6953a2
Jethro <jethrotiago@hotmail.com>
2023-02-07 13:59:43 -0300

    Importando icones de React Icons e Mui

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,20 @@
 import React from 'react';
 import Logo from '../Assets/logo.png';
+import { CgShoppingCart } from 'react-icons/Cg';
+import { HiOutlineBars3 } from 'react-icons/Hi';
+import {
+  Box,
+  Drawer,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from '@mui/material';
+import HomeIcon from "@mui/icons-material/Home";
+import InfoIcon from "@mui/icons-material/Info";
+import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
+import PhoneRoundedIcon from '@mui/icons-material/PhoneRounded';
+import ShoppingCartRoundedIcon from '@mui/icons-material/ShoppingCartRounded';
 
 const Navbar = () => {
   return (
