7f3726e190f9a854c58e07c9ba360e059db2201e
Jethro <jethrotiago@hotmail.com>
2023-02-07 16:30:01 -0300

    Criando estrutura da Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from '../Assets/logo.png';
 import { CgShoppingCart } from 'react-icons/Cg';
 import { HiOutlineBars3 } from 'react-icons/Hi';
@@ -17,9 +17,49 @@ import PhoneRoundedIcon from '@mui/icons-material/PhoneRounded';
 import ShoppingCartRoundedIcon from '@mui/icons-material/ShoppingCartRounded';
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+
+  const [openMenu, setOpenMenu] = useState(false);
+  const menuOptions = [
+    {
+      text:"Home",
+      icon:<HomeIcon/>
+    },
+    {
+      text:"Sobre",
+      icon:<InfoIcon/>
+    },
+    {
+      text:"Depoimentos",
+      icon:<CommentRoundedIcon/>
+    },
+    {
+      text:"Contato",
+      icon:<PhoneRoundedIcon />
+    },
+    {
+      text:"Carrinho",
+      icon:<ShoppingCartRoundedIcon />
+    },
+  ];
+
+  return <nav>
+    <div className='nav-logo-container'>
+      <img src= { Logo } alt='Logo da Pizzaria' />      
+    </div>
+    <div className='navbar-links-container'>
+      <a href=''>Home</a>
+      <a href=''>Sobre</a>
+      <a href=''>Depoimentos</a>
+      <a href=''>Contato</a>
+      <a href=''>
+        <CgShoppingCart className='navbar-cart-icon' />
+      </a>
+      <button className='primary-button'>Compre Agora!</button>
+    </div>
+    <div className='navbar-menu-container'>
+      <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
+    </div>
+    </nav>
 }
 
 export default Navbar;
