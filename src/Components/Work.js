35bc61219fc51a234928fa7ace32e7f731a75866
Jethro <jethrotiago@hotmail.com>
2023-02-08 09:40:23 -0300

    Finalizando códigos para Work.js

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -21,7 +21,28 @@ const Work = () => {
             text: "Se você mora no bairro de Cajazeiras ou adjacências, entregaremos a pizza bem rapidinho em sua residência!",
         },
     ];
-    return <div>Work</div>
+    return <div className='work-section-wrapper'>
+        <div className='work-section-top'>
+            <p className='primary-subheading'>EXPRESS CAJACITY</p>
+            <h1 className='primary-subheading'>Como trabalhamos</h1>
+            <p className='primary-text'>
+                De terça a quinta das 18:00 às 22h e de sexta a domingo das 18:00 às 22:40h! SÓ DELIVERY!
+            </p>
+        </div>
+        <div className='work-section-bottom'>
+            {
+                workInfoData.map((data) => (
+                    <div className='work-section-info'>
+                        <div className='info-boxes-img-container'>
+                            <img src={data.image} alt="Uma imagem ilustrativa do que esta sendo exposto"></img>
+                        </div>
+                        <h2>{data.title}</h2>
+                        <p className={data.text}></p>
+                    </div>
+                ))
+            }
+        </div>
+    </div>
 };
 
 export default Work;
