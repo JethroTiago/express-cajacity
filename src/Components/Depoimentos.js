b69015b841d5c6c551ea92b6156302054934699c
Jethro <jethrotiago@hotmail.com>
2023-02-08 12:00:02 -0300

    Criado componente Contato.js

--- src/Components/Depoimentos.js
+++ src/Components/Depoimentos.js
@@ -6,17 +6,17 @@ const Depoimentos = () => {
   return <div className='work-section-wrapper'>
     <div className='work-section-top'>
         <p className='primary-subheading'>Depoimentos</p>
-        <h1 className='primary-heading'>O que os clientes falam sobre a Express Cajacity Pizzaria</h1>
+        <h1 className='primary-heading'>Opinião do cliente</h1>
         <p className='primary-text'>
             "A melhor pizza da região! Uma pizza como você nunca viu! As bordas são diferentes de todas as outras que já experimentei! E os sabores autorais, que homenageiam regiões do bairro de Cajazeiras?! São fantásticos!!!"
         </p>
     </div>
-    <div className='testimonials-section-bottom'>
+    <div className='testimonial-section-bottom'>
         <img src={ ProfilePic } alt='Foto do instagram de Gaby'></img>
         <p>
             Nutricionista • Criadora de conteúdo digital • Baiana em Curitiba • Vida com humor • Maternidade • Dicas • Embaixadora @nblack_21
         </p>
-        <div className='testimonial-stars-container'>
+        <div className='testimonials-stars-container'>
             <AiFillStar />
             <AiFillStar />
             <AiFillStar />
