f07af1f61d63a5d0a4b59cd1dd0f5db7d16d08c4
Jethro <jethrotiago@hotmail.com>
2023-02-07 21:26:06 -0300

    Criado códigos para o Work.js

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import PickPizza from "../Assets/pick-pizza-image.png";
+import ChoosePizza from "../Assets/choose-image.png";
+import DeliveryPizza from "../Assets/delivery-image.png";
 
 const Work = () => {
-  return <div>Work</div>
+    const workInfoData = [
+        {
+            image: PickPizza,
+            title: "Bateu a fome?",
+            text: "Pizza é um prato que combina com tudo! É um dos alimentos mais consumidos do mundo!",
+        },
+        {
+            image: ChoosePizza,
+            title: "Escolha uma pizza",
+            text: "Temos um imenso cardápio com uma quantidade gigante de sabores e tamanhos. É só clicar e escolher seu sabor favorito!",
+        },
+        {
+            image: DeliveryPizza,
+            title: "Entregamos em sua casa",
+            text: "Se você mora no bairro de Cajazeiras ou adjacências, entregaremos a pizza bem rapidinho em sua residência!",
+        },
+    ];
+    return <div>Work</div>
 };
 
-export default Work
+export default Work;
